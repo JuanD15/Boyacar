2d424102b6d583725cfa9aed685447282ae200eb
JuanD15 <80301917+JuanD15@users.noreply.github.com>
2024-05-25 20:49:42 -0500

    signUp, signIn services available

--- src/services/SignInService.js
+++ src/services/SignInService.js
@@ -1,9 +1,38 @@
-import { supabase } from "./ConnectService.mjs"
+import { supabase } from "./ConnectService.mjs";
 
 export const signInWithEmailAndPassword = async (email, password) => {
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email, password,
-    })
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
 
-    return { data, error }
-}
+  /**
+   * 
+   */
+  const user = data.user;
+  const userId = data.user.id;
+  supabase.auth.signOut();
+
+  /**
+   * 
+   */
+  let { data: profile, errorProf } = await supabase
+    .from("Profile")
+    .select("*")
+    .eq("user_id", userId);
+
+  /**
+   * 
+   */
+  const profId = profile[0].profile_id;
+
+  /**
+   * 
+   */
+  let { data: person, errorPerson } = await supabase
+    .from("Person")
+    .select("*")
+    .eq("person_id", profId);
+
+  return { data, error };
+};
--- src/services/SignUpService.js
+++ src/services/SignUpService.js
@@ -1,8 +1,49 @@
-import { supabase } from "./ConnectService.mjs"
+import { supabase } from "./ConnectService.mjs";
 
 export const signUpWithEmailAndPassword = async (email, password) => {
-    const { data, error } = await supabase.auth.signUp({
-        email, password
-    })
-    return { data, error }
-}
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  /**
+   * Se guarda el id del usuario y se cierra la sesion 
+   * para permitir consultas con la base de datos
+   */
+  const uId = user.user.id;
+  supabase.auth.signOut();
+
+  /**
+   * 
+   */
+  const { person, errorPers } = await supabase
+    .from("Person")
+    .insert([
+      {
+        person_id: id,
+        person_name: name,
+        person_last_name: last_name,
+        person_gender: gender,
+        birth_date: birthDate,
+        person_photo: idPhoto,
+        id_document_photo: "URL",
+        phone_number: phoneNumber,
+      },
+    ])
+    .select();
+
+  /**
+   * 
+   */
+  const { profile, errorProf } = await supabase
+    .from("Profile")
+    .insert([
+      {
+        profile_id: id,
+        user_id: uId,
+      },
+    ])
+    .select();
+
+  return { data, error };
+};
